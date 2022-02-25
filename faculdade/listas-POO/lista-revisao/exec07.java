d10658610d6892fab4d1c4e9e6041fb357d06869
Pedro Ivo <peustratt@gmail.com>
2022-02-25 00:17:20 -0300

    contador de substring em string

---
+++ faculdade/listas-POO/lista-revisao/exec07.java
@@ -0,0 +1,28 @@
+import java.io.Console;
+import java.util.Scanner;
+
+// Faça um programa que receba uma frase e uma palavra.
+// Calcule e mostre a quantidade de vezes que a palabra digitada aprece na frase.
+
+public class exec07 {
+    static Scanner in = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        String frase = "Calcule e mostre a quantidade de vezes que a palabra digitada aprece na frase. ", str = "";
+        int tamanhoStr = str.length();
+        int indexFinal = frase.length() - tamanhoStr + 1;
+        int count = 0;
+
+        System.out.println("Digite a string: ");
+
+
+        for (int i = 0; i < indexFinal; i++) {
+            if (frase.substring(i, i + tamanhoStr).equals(str)) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+
+}
